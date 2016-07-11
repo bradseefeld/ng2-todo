d982dc6b82e2e4bcaa9ca38281f74c19fab60c6b
Brad Seefeld <bradley.seefeld@gmail.com>
2016-07-11 15:47:01 -0700

    Add our first test

---
+++ app/app.spec.ts
@@ -0,0 +1,8 @@
+import { AppComponent } from "./app.component";
+
+describe("AppComponent", () => {
+  it("constructs", () => {
+    let app = new AppComponent();
+    expect(app).toBeDefined();
+  });
+});
